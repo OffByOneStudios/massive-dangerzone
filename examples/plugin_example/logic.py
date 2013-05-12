aac68fb922a1ff965272702ff86c9325800854e0
Mason Bially <mason.bially@gmail.com>
2013-05-11 19:14:29 -0700

    Adding more to logic.py

--- examples/plugin_example/logic.py
+++ examples/plugin_example/logic.py
@@ -1,6 +1,17 @@
 import nature
+import magic.interface
 
 from nature.animal.bird import BirdBase as BirdBaseOld
 BirdBaseNew = nature.__domain__.import("animal.bird", version="0.6.0")
 
+def do_some_stuff()
+    bird1 = BirdBaseOld()
+    print bird1.sound() # "Kaww!"
 
+    bird2 = BirdBaseNew()
+    if isinstance(bird2, nature.kingdom.animal.IAnimal):
+        raise Exception("Should not happen")
+    print bird2.sound() # "Kaaw"
+
+    cat = nature.animal.cat.CatBase()
+    print cat.sound() # "Meow"
