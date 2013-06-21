1c2bcda33e5b8ca10863b830ef5254a156e9e72e
Clark Rinker <cgrinker@gmail.com>
2013-06-21 05:02:05 -0700

    End of workday

--- examples/simple_example/plugins/a/a.c
+++ examples/simple_example/plugins/a/a.c
@@ -1,8 +1,8 @@
 #include "madz.h"
 
+#define MADZTHIS(object){}
+#define MADZTHISTYPE(object)
 
 float MADZTHIS(distance)(MADZTHISTYPE(Point2d) a, MADZTHISTYPE(Point2d) b){
 	return (a.x-b.x) + (a.y - b.y);
-
-
 }
