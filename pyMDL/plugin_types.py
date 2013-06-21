22763d3baaf7b38a1f27cab57c444071a6ce7c04
Clark Rinker <cgrinker@gmail.com>
2013-06-21 02:16:04 -0700

    Adding Docstrings to plugin_types.py

--- pyMDL/plugin_types.py
+++ pyMDL/plugin_types.py
@@ -12,7 +12,7 @@ class Type(object):
     def __init__(self, value=None):
         self.value = value
 
-        
+
 class TypeNone(Type):
     """Void, as in No Return Type"""
     pass
@@ -24,46 +24,57 @@ class TypeVoid(Type):
 
 
 class TypeInt8(Type):
+    """8 Bit Signed Integer"""
     pass
 
 
 class TypeInt16(Type):
+    """16 Bit Signed Integer"""
     pass
 
 
 class TypeInt32(Type):
+    """32 Bit Signed Integer"""
     pass
 
 
 class TypeInt64(Type):
+    """64 Bit Signed Integer"""
     pass
 
 
 class TypeUInt8(Type):
+    """8 Bit Unsigned Integer"""
     pass
 
 
 class TypeUInt16(Type):
+    """16 Bit Unsigned Integer"""
     pass
 
 
 class TypeUInt32(Type):
+    """32 Bit Unsigned Integer"""
     pass
 
 
 class TypeInt64(Type):
+    """64 Bit Unsigned Integer"""
     pass
 
 
 class TypeFloat32(Type):
+    """32 Bit Floating Point Value"""
     pass
 
 
 class TypeFloat64(Type):
+    """64 Bit Floating Point Value"""
     pass
 
 
 class TypeChar(Type):
+    """8 Bit Latin_1 Character"""
     pass
 
 class TypeTypedef(Type):
@@ -78,7 +89,7 @@ class TypeTypedef(Type):
 
 class TypeStructType(Type):
     """A Struct Declaration"""
-    
+
     def __init__(self, value):
         """C Struct Representation
         Args:
@@ -98,12 +109,13 @@ class TypeStructVar(Type):
 
 
 class TypePtr(Type):
+    """A Mutable Pointer"""
     pass
 class TypeFunction(Type):
-
+    """A Function"""
     def __init__(self, return_type = TypeNone(), args={}, attributes = []):
         self.return_type = return_type
         self.attributes = attributes
         self.args = args
-        
-    
+
+
