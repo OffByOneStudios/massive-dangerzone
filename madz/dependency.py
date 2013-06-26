d70ca8dfb36287aa0c6b11f7845e4b1b70015bbb
Alexi Pors <alexi.pors@gmail.com>
2013-06-26 14:45:24 -0700

    Creating dependcies checker in dependencies.py

--- madz/dependency.py
+++ madz/dependency.py
@@ -2,6 +2,55 @@
 @OffbyOneStudios 2013
 Code to create and traverse dependency graphs of plugins.
 """
+
+import os.path, time
+
 class Dependency(object):
     """Generates a dependency graph of plugins allowing for generation in the correct order."""
-    pass
+
+    def __init__(self, dependencies, targets):
+        self.dependencies = dependencies
+        self.targets = targets
+        self._unsatisfied_targets = self.check()
+
+    def check(self):
+        """Checks that there are no targets which are older than the dependencies."""
+        newest_dependency = time.ctime(0)
+
+        #Find the newest dependency
+        for d in self.dependencies:
+            temp = time.ctime(os.path.getmtime(d))
+            if temp > newest_dependency:
+                newest_dependency = temp
+
+        unsatisfied_targets = []
+
+        #Verify each target is newer than the newest dependency, add targets
+        #which are older than their dependences to the unsatisfied_targets list
+        for t in self.targets:
+            if time.ctime(os.path.getmtime(t)) <= newest_dependency:
+                unsatisfied_targets.append(t)
+
+        return unsatisfied_targets
+
+    def __bool__(self):
+        if len(self._unsatisfied_targets) == 0:
+            return True
+        else:
+            return False
+
+    __nonzero__ = __bool__
+
+    def get_unsatisfied_targets(self):
+        return self._unsatisfied_targets
+
+"""
+#Example calls to Dependency
+
+Dep = Dependency(["loader.py"],["builder.py"])
+
+print Dep.get_unsatisfied_targets()
+
+if Dep:
+    print "hi"
+"""
