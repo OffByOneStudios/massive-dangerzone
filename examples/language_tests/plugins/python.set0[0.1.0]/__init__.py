1fe0992ebebfe4c2dd417bd2c20675de671f2875
Clark Rinker <cgrinker@gmail.com>
2013-07-17 19:29:52 -0700

    Adding implementations for python test functions.

--- examples/language_tests/plugins/python.set0[0.1.0]/__init__.py
+++ examples/language_tests/plugins/python.set0[0.1.0]/__init__.py
@@ -1,1 +1,16 @@
-#Fill In Module Code in this File
+
+def test1_func():
+    print("Hello From test1_func")
+
+def test2_func0():
+    return 0
+
+def test2_func1(a, b):
+    return a+b
+
+def test3_func():
+    print("Hello From test3_func")
+
+def test5_func():
+    #TODO(Clark) This function needs access to _madz.py's type declarations.
+    print("Hope You're ready for some crash")
