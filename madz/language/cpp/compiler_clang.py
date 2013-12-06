070418301fe9db694a22d94ae799d0566d8aa6c0
Mason Bially <mason.bially@gmail.com>
2013-12-06 15:29:32 -0800

    Moving c++ to c++11 standard. Adding better refrences to wrapgen.

--- madz/language/cpp/compiler_clang.py
+++ madz/language/cpp/compiler_clang.py
@@ -9,3 +9,8 @@ class ClangCompiler(CClangCompiler):
         
     def binary_name_binary_compiler(self):
         return "clang++"
+
+    def _gen_specifc_compile_flags(self):
+        """Returns a list of the language specific compiler flags. Must be overloaded if a different language."""
+        return \
+            ["-std=c++11"]
--- madz/language/cpp/compiler_gcc.py
+++ madz/language/cpp/compiler_gcc.py
@@ -9,3 +9,8 @@ class GCCCompiler(CGCCCompiler):
         
     def binary_name_binary_compiler(self):
         return "g++"
+
+    def _gen_specifc_compile_flags(self):
+        """Returns a list of the language specific compiler flags. Must be overloaded if a different language."""
+        return \
+            ["-std=c++11"]
