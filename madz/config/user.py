4528d5d3aca7fc6bf6f5897fbef967eaecea6ca5
Clark Rinker <cgrinker@gmail.com>
2014-05-17 23:51:03 -0700

    Fixing UserConfig load to throw an exception when it fails to load the config.

--- madz/config/user.py
+++ madz/config/user.py
@@ -28,16 +28,12 @@ class UserConfig(BaseConfig):
         import imp
         if (not (filename is None)) and os.path.exists(filename):
             with open(filename) as module_file: #TODO(Mason): Figure out this name
-                try:
-                    module = imp.load_module("a_config", module_file, filename, ('.py', 'r', imp.PY_SOURCE))
-                    config = getattr(module, "config")
-                    if isinstance(config, UserConfig):
-                        return config
-                    else:
-                        raise UserConfigNotFoundError("Did not find a UserConfig in the 'config' var of '{}'.")
-                except:
-                    tb_string = "\n\t".join(("".join(traceback.format_exception(*sys.exc_info()))).split("\n"))
-                    logger.error("Failed to load user config from file '{}':\n\t{}".format(filename, tb_string))
+                module = imp.load_module("a_config", module_file, filename, ('.py', 'r', imp.PY_SOURCE))
+                config = getattr(module, "config")
+                if isinstance(config, UserConfig):
+                    return config
+                else:
+                    raise UserConfigNotFoundError("Did not find a UserConfig in the 'config' var of '{}'.")
         logger.info("Skipping user config. File not found '{}'.".format(filename))
                     
         return cls.make_default()
